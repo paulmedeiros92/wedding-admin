f8382e3c5eda72f8972e98bd7a6f853bf15d5b0a
PaulMed <paulmedeiros92@gmail.com>
2022-02-25 10:42:49 -0800

    Add vue material theme global file and update vue project to support scss

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,8 @@ export default {
   }
 };
 </script>
-<style>
+<style lang="scss">
+@import "./scss/theme.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

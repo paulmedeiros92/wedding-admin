fa1820cce7df24f59ee59b1cab3caa1a826f85e0
PaulMed <paulmedeiros92@gmail.com>
2022-03-08 19:26:18 -0800

    Boolean sort working

--- src/components/HouseholdAttendees.vue
+++ src/components/HouseholdAttendees.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="guestList">
-    <md-table v-model="attendees" md-sort="isAttending" md-sort-order="asc" md-card>
+    <md-table
+      v-model="attendees"
+      :md-sort="currentSort"
+      @update:mdSort="(value) => currentSort = value"
+      :md-sort-order="currentSortOrder"
+      @update:mdSortOrder="(value) => currentSortOrder = value"
+      :md-sort-fn="customSort"
+      md-card>
       <md-table-toolbar>
         <h1 class="md-title">{{title}}</h1>
       </md-table-toolbar>
-      <md-table-row slot="md-table-row" slot-scope="{ item, index }">
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
         <md-table-cell md-label="Hashword" md-sort-by="hashWord">{{ item.hashWord }}</md-table-cell>
         <md-table-cell md-label="First Name" md-sort-by="firstName">{{ item.firstName }}</md-table-cell>
         <md-table-cell md-label="Last Name" md-sort-by="lastName">{{ item.lastName }}</md-table-cell>
@@ -42,9 +49,25 @@ export default {
       default: () => [],
     }
   },
+  data: () => ({
+    currentSort: 'isAttending',
+    currentSortOrder: 'asc',
+  }),
   methods: {
-    deleteEvent(index) {
-      this.$emit('delete', index);
+    customSort (value) {
+      return value.sort((a, b) => {
+        const sortBy = this.currentSort;
+
+        if (this.currentSortOrder === 'desc') {
+          return a[sortBy].localeCompare ?
+            a[sortBy].localeCompare(b[sortBy])
+            : +a[sortBy] - +b[sortBy];
+        }
+
+        return b[sortBy].localeCompare ?
+          b[sortBy].localeCompare(a[sortBy])
+          : +b[sortBy] - +a[sortBy];
+      });
     }
   },
 };

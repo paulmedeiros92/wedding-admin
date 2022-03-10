b9beb2bcb306bc3b75f16f4211794c301e6346b0
PaulMed <paulmedeiros92@gmail.com>
2022-03-09 17:44:45 -0800

    WIP getting common table working

---
+++ src/common/Table.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="common-table">
+    <md-table
+      v-model="searchedItems"
+      :md-sort="currentSort"
+      @update:mdSort="(value) => currentSort = value"
+      :md-sort-order="currentSortOrder"
+      @update:mdSortOrder="(value) => currentSortOrder = value"
+      :md-sort-fn="customSort"
+      md-card>
+      <md-table-toolbar>
+        <div class="md-toolbar-section-start">
+          <h1 class="md-title">{{title}}</h1>
+        </div>
+        <md-field md-clearable class="md-toolbar-section-end">
+          <md-input placeholder="Search by..." v-model="search" @input="searchOnTable" />
+        </md-field>
+      </md-table-toolbar>
+      <md-table-empty-state
+        md-label="Nothing to see here..."
+        :md-description="`Nothing found for this '${search}' query. Try a different search term.`">
+      </md-table-empty-state>
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell
+          v-for="column of columns"
+          :key="column.label"
+          :md-label="column.label"
+          :md-sort-by="column.key">
+          <md-icon
+            v-if="column.isBoolean"
+            :class="{ green: item[column.key], boolean}">
+            {{item[column.key] ? "check" : "close"}}
+          </md-icon>
+          <span v-else>{{ item[column.key] }}</span>
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonTable',
+  props: {
+    title: String,
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+    columns: {
+      type: Object,
+      required: true,
+    },
+    currentSort: {
+      type: String,
+      required: true,
+    },
+    currentSortOrder: {
+      type: String,
+      validator: function(value) {
+        return ['asc', 'desc'].includes(value);
+      },
+      default: 'asc',
+    }
+  },
+  data: () => ({
+    search: null,
+    searchedItems: [],
+  }),
+  created() {
+    this.searchedItems = this.items;
+  },
+  methods: {
+    customSort (value) {
+      return value.sort((a, b) => {
+        const sortBy = this.currentSort;
+
+        if (this.currentSortOrder === 'desc') {
+          return a[sortBy].localeCompare ?
+            a[sortBy].localeCompare(b[sortBy])
+            : +a[sortBy] - +b[sortBy];
+        }
+
+        return b[sortBy].localeCompare ?
+          b[sortBy].localeCompare(a[sortBy])
+          : +b[sortBy] - +a[sortBy];
+      });
+    },
+    searchOnTable () {
+      if (this.search) {
+        this.searchedItems = this.items.filter((attendee) => JSON.stringify(attendee).toLowerCase().includes(this.search.toLowerCase()));
+      } else {
+        this.searchedItems = this.items;
+      }
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.md-toolbar-section-end {
+  flex: 0;
+}
+.boolean {
+  color: red;
+}
+.green {
+  color: green;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,6 +5,11 @@
       title="Guests"
       :attendees="$store.getters.attendees"
       :isLoading="$store.getters.isLoading" />
+    <common-table
+      title="Guests"
+      :items="attendees"
+      :labels="labels"
+      currentSort=""/>
   </div>
 </template>
 
@@ -12,12 +17,27 @@
 // @ is an alias to /src
 import Statistics from '@/components/Statistics.vue';
 import HouseholdAttendees from '@/components/HouseholdAttendees';
+import CommonTable from '@/common/Table.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'dashboard',
   components: {
     Statistics,
     HouseholdAttendees,
+    CommonTable,
+  },
+  data: () => ({
+    labels: [
+      'Hashword', 'First Name', 'Last Name', 'Confirmed?', 'Viewed?', 'Address',
+      'Food Selection', 'Child?', 'Notes'
+    ]
+  }),
+  computed: {
+    ...mapGetters([
+      'attendees',
+      'isLoading'
+    ])
   }
 };
 </script>
